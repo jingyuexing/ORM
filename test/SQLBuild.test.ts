3269cd72f9c0ff163353d2e3a3e569c9205eacb4
jingyuexing <MayorTalor@163.com>
2023-02-04 14:33:48 +0800

    ✅ test: append test case     - `testing update by where`     - `testing convert values`     - `testing convert values input arrary`     - `testing typeis array`     - `testing typeis object`     - `testing typeis map`     - `testing typeis number`     - `testing typeis date`

--- test/SQLBuild.test.ts
+++ test/SQLBuild.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, test } from "vitest";
-import { generatorColumns, generatorParameters, Select, Update } from "@/SQLBuilder";
+import { generatorColumns, generatorParameters, Select, Update, typeIs, convertValue } from "@/SQLBuilder";
 
 class User {
     username: string = "";
@@ -21,6 +21,9 @@ describe("test sql builder", () => {
         expect(generatorColumns(user)).eq("(username,age,address)");
     });
     it("testing select", () => {
+        // select syntax is:
+        // select * from <tablename>
+        // select (column1,column1=2) from <tablename>;
         expect(Select<User>({
             select: {
                 username: true
@@ -29,12 +32,59 @@ describe("test sql builder", () => {
         })).eq("select (username) from User")
     });
     it("testing update", () => {
+        // update syntax is:
+        //   update <tablename> set column1=value,column2=value2,column3=value3 where id=value4
         expect(Update<User>({
             data: {
                 username: "李四",
                 age: 23,
             },
             entity: user
-        })).toBe("update User set username=李四,age=23")
+        })).toBe(`update User set username="李四",age=23;`)
+    });
+    it("testing update by where", () => {
+        // update syntax is:
+        //   update <tablename> set column1=value,column2=value2,column3=value3 where id=value4
+        expect(Update<User>({
+            data: {
+                username: "李四",
+                age: 23,
+            },
+            entity: user,
+            where:{
+                select:{
+                    username:"张三"
+                }
+            }
+        })).toBe(`update User set username="李四",age=23 where username="张三";`)
+    });
+    it("testing convert values",()=>{
+        expect(convertValue(
+            "(?,?,?,?)",
+            ["张三","赵四","王五","孙六"]
+        )).toBe(`("张三","赵四","王五","孙六")`)
+    })
+    it("testing convert values input arrary",()=>{
+        expect(convertValue(
+            [
+                "(?,?,?,?)",
+                ["张三","赵四","王五","孙六"]
+            ]
+        )).toBe(`("张三","赵四","王五","孙六")`)
+    })
+    it("testing typeis array",()=>{
+        expect(typeIs([])).toBe("array")
+    })
+    it("testing typeis object",()=>{
+        expect(typeIs({})).toBe("object")
+    });
+    it("testing typeis map",()=>{
+        expect(typeIs("")).toBe("string")
+    })
+    it("testing typeis number",()=>{
+        expect(typeIs(12)).toBe("number")
+    });
+    it("testing typeis date",()=>{
+        expect(typeIs(new Date())).toBe("date")
     })
 })
